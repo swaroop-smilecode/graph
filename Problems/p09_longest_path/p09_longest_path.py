3a480661fb009422f202c27afc15f1ea3648f940
swaroop <swaroopiit@gmail.com>
2024-10-29 11:51:29 +0530

    Solved some more problems

--- Problems/p09_longest_path/p09_longest_path.py
+++ Problems/p09_longest_path/p09_longest_path.py
@@ -2,24 +2,24 @@ def longest_path(graph):
     visited = set()
     longest = float("-inf")
     for node in graph:
-        curr_longest = _explore_dfs(graph, node, visited)
+        curr_longest = _explore_dfs(graph, node, 0, visited)
         longest = max(curr_longest, longest)
     return longest 
 
-def  _explore_dfs(graph, node, visited):
+def  _explore_dfs(graph, node, distance, visited):
     # Base case
     if graph[node] == []:
         return 0 
     if node in visited:
-        return 0
+        return distance
     else:
         visited.add(node)
-    distance = 1
+        distance = 1
 
-    neighbor_distances = set()
     # Recursive calls
+    neighbor_distances = set()
     for neighbor in graph[node]:
-        neighbor_distances.add(_explore_dfs(graph, neighbor, visited)) 
+        neighbor_distances.add(_explore_dfs(graph, neighbor, distance, visited)) 
     max_distance_neighbor = max(neighbor_distances)
     distance += max_distance_neighbor
 
