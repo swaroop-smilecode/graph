b7d422da1fcfd1dea91aa13c0639f03a2586bd56
swaroop <swaroopiit@gmail.com>
2024-10-31 08:17:49 +0530

    Solved some more problems

---
+++ Problems/p12_has_cycle/p12_has_cycle.py
@@ -0,0 +1,28 @@
+def has_cycle(graph):
+    grey = set()
+    black = set()
+    for node in graph:
+        if _has_cycle(graph, node, grey, black) == True:
+            return True
+    return False
+
+def _has_cycle(graph, node, grey, black):
+    if node in black:
+        return False
+    
+    if node in grey:
+        return True
+    grey.add(node)
+    for neighbor in graph[node]:
+        if _has_cycle(graph, neighbor, grey, black) == True:
+            return True
+    grey.remove(node)
+    black.add(node)
+    
+    return False
+
+has_cycle({
+  "a": ["b"],
+  "b": ["c"],
+  "c": ["a"],
+}) # -> True
